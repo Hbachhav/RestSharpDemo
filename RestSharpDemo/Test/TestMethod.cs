dcb9213e712f712ccf2c8b38a512422d7ffed87d
Hitesh Bachhav <bachhav.hitesh7@live.com>
2021-07-15 18:54:32 +0530

    Add project files.

---
+++ RestSharpDemo/Test/TestMethod.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using RestSharpDemo.DTO;
+
+namespace RestSharpDemo
+{
+    [TestClass]
+    public class TestRun
+    {
+
+        [TestInitialize]
+        public void setupInit()
+        {
+
+          
+        }
+
+        [TestMethod]
+        [DataRow("https://reqres.in/api/users/3")]
+
+
+        public void GetUser(string url)
+        {
+           
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            GetUserDTO UserResponse =  new JsonDeserializer().Deserialize<GetUserDTO>(response);
+
+            Assert.AreEqual(3, UserResponse.Data.Id);
+           Assert.AreEqual("Emma", UserResponse.Data.FirstName);
+
+        }
+
+        [TestMethod]
+        [DataRow("https://reqres.in/api/users", @"{
+                                ""name"": ""Rahul"",
+                                 ""job"": ""Member"" 
+                               }")]
+
+        public void createUser(string url, dynamic payload)
+        {
+               
+       
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.POST);
+            
+            request.AddJsonBody(payload);
+            IRestResponse response = client.Execute(request);
+            CreateUserDTO UserResponse = new JsonDeserializer().Deserialize<CreateUserDTO>(response);
+
+            Assert.AreEqual("Rahul", UserResponse.Name);
+
+        }
+
+        [TestMethod]
+        [DataRow("https://reqres.in/api/users/2", @"{
+                                ""name"": ""update Rahul"",
+                                 ""job"": ""updated Member"" 
+                               }")]
+
+        public void updateUser(string url, dynamic payload)
+        {
+
+
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.PUT);
+
+            request.AddJsonBody(payload);
+            IRestResponse response = client.Execute(request);
+            UpdateUserDTO UserResponse = new JsonDeserializer().Deserialize<UpdateUserDTO>(response);
+
+            Assert.AreEqual("update Rahul", UserResponse.Name);
+
+        }
+
+        [TestMethod]
+        [DataRow("https://reqres.in/api/users/2")]
+
+
+        public void DeleteUser(string url)
+        {
+
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.DELETE);
+            IRestResponse response = client.Execute(request);
+
+            Assert.AreEqual(true,response.IsSuccessful );
+           
+
+        }
+
+    }
+}
---
+++ RestSharpDemo/TestData/CreateUserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestSharpDemo
+{
+
+    public partial class CreateUserDTO
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public long Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+
+
+}
+
+
+   
+
